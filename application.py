bfe625ca64ef971314db28d473a3d6517a93f7f0
Scott McClung <scott.mcclung@gmail.com>
2023-11-13 22:04:29 -0500

    New commit

--- application.py
+++ application.py
@@ -3,4 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Hello Best Bike App!</h1></body></html>\n"
+    return "<html><body><h1>Hello Scott McClung!</h1></body></html>\n"
+
+@app.route("/hellorld")
+def helloworld():
+    return "Hellorld!\n"
+
