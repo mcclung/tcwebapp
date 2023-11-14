4e7151da6a1fe15438fa50d5dc69046080f9b180
Scott McClung <scott.mcclung@gmail.com>
2023-11-13 22:11:23 -0500

    Update routes for test app

--- application.py
+++ application.py
@@ -1,5 +1,9 @@
+import datetime
 from flask import Flask
+
 app = Flask(__name__)
+now = datetime.datetime.now()
+formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
 
 @app.route("/")
 def hello():
@@ -8,4 +12,8 @@ def hello():
 @app.route("/hellorld")
 def helloworld():
     return "Hellorld!\n"
+
+@app.route("/test")
+def helloworld():
+    return formatted_date
 
